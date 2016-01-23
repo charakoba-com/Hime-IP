1037e6edf242713b29635eaba458092ed7c9eb9e
chamaharun <chamaharun@outlook.com>
2016-01-23 14:01:17 +0900

    fixed file path problem

--- ipcheck.py
+++ ipcheck.py
@@ -1,16 +1,18 @@
 # -*- coding:utf-8 -*-
 import urllib2
 import tweet
-
+import os
 
 
 current_ip_val = urllib2.urlopen('http://inet-ip.info/ip').read()
+ip_file = os.path.join(os.path.dirname(__file__), 'global.ip')
 
-with open('global.ip','r') as recorded_ip:
+
+with open('ip_file','r') as recorded_ip:
 	recorded_ip_val = recorded_ip.read()
 
 if (current_ip_val != recorded_ip_val):
-	with open('global.ip','w') as recorded_ip:
+	with open(ip_file,'w') as recorded_ip:
 		recorded_ip.write(current_ip_val)
 	text = "@youraccount tweet message here\nIP:" + current_ip_val
 	tweet.tweet(text)
